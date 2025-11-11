b4fd2914a0cb3b3fed2f4b92dead5eced1033073
GrooveypenguinX <jakechase2110@gmail.comX>
2025-11-11 10:14:19 -0500

    added CJ's slothelpers

---
+++ WTT-ServerCommonLib/Helpers/SlotHelper.cs
@@ -0,0 +1,38 @@
+﻿using SPTarkov.Server.Core.Models.Common;
+using SPTarkov.Server.Core.Models.Eft.Common.Tables;
+
+namespace WTTServerCommonLib.Helpers;
+
+public class SlotHelper
+{
+    public void ReplaceSlotFilters(TemplateItem item, int slotIndex, int filterIndex, HashSet<MongoId> ids)
+    {
+        var slot = GetSlotAtIndex(item, slotIndex);
+        var filter = GetSlotFilterAtIndex(slot, filterIndex);
+
+        filter.Filter = ids;
+    }
+    private Slot GetSlotAtIndex(TemplateItem item, int index, bool isCartridge = false)
+    {
+        var slots = isCartridge ? item.Properties?.Cartridges?.ToArray() : item.Properties?.Slots?.ToArray();
+
+        if (index >= 0 && index < slots?.Length)
+        {
+            return slots[index];
+        }
+
+        throw new IndexOutOfRangeException($"Index on item slot property `{item.Name}` is out of range");
+    }
+
+    private SlotFilter GetSlotFilterAtIndex(Slot slot, int index)
+    {
+        var slotFilter = slot.Properties?.Filters?.ToArray() ?? [];
+
+        if (index >= 0 && index < slotFilter.Length)
+        {
+            return slotFilter[index];
+        }
+
+        throw new IndexOutOfRangeException($"Index on slot property `{slot.Name}` is out of range");
+    }
+}
