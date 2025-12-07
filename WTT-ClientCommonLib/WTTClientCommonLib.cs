c5ce953658df143d8da50fe7e646a955310b132f
GrooveypenguinX <jakechase2110@gmail.comX>
2025-12-07 15:48:14 -0500

    version bump 207

--- WTT-ClientCommonLib/WTTClientCommonLib.cs
+++ WTT-ClientCommonLib/WTTClientCommonLib.cs
@@ -17,7 +17,7 @@ using WTTClientCommonLib.Services;
 namespace WTTClientCommonLib;
 
 [BepInDependency("com.fika.core", BepInDependency.DependencyFlags.SoftDependency)]
-[BepInPlugin("com.wtt.commonlib", "WTT-ClientCommonLib", "2.0.6")]
+[BepInPlugin("com.wtt.commonlib", "WTT-ClientCommonLib", "2.0.7")]
 public class WTTClientCommonLib : BaseUnityPlugin
 {
     private static CommandProcessor.CommandProcessor _commandProcessor;
