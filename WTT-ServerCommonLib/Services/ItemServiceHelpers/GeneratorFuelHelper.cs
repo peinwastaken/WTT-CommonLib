9cbc1756d5a939962013b3bd74edbd4383aacc27
kittycat2002 <josh.masters.02@gmail.com>
2025-12-07 14:55:59 -0500

    Fixed AddGeneratorFuel

--- WTT-ServerCommonLib/Services/ItemServiceHelpers/GeneratorFuelHelper.cs
+++ WTT-ServerCommonLib/Services/ItemServiceHelpers/GeneratorFuelHelper.cs
@@ -1,4 +1,5 @@
 ﻿using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.Models.Eft.Hideout;
 using SPTarkov.Server.Core.Models.Enums;
 using SPTarkov.Server.Core.Models.Utils;
 using SPTarkov.Server.Core.Services;
@@ -10,40 +11,38 @@ namespace WTTServerCommonLib.Services.ItemServiceHelpers;
 [Injectable]
 public class GeneratorFuelHelper(ISptLogger<GeneratorFuelHelper> logger, DatabaseService databaseService)
 {
-    public void AddGeneratorFuel(CustomItemConfig itemConfig, string itemId)
-    {
-        var hideout = databaseService.GetHideout();
-        var generator = hideout.Areas.Find(a => a.Id == "5d3b396e33c48f02b81cd9f3");
-        var validStages = itemConfig.GeneratorFuelSlotStages;
+	public void AddGeneratorFuel(CustomItemConfig itemConfig, string itemId)
+	{
+		var hideout = databaseService.GetHideout();
+		var generator = hideout.Areas.Find(a => a.Id == "5d3b396e33c48f02b81cd9f3");
+		var validStages = itemConfig.GeneratorFuelSlotStages;
 
-        if (generator == null)
-        {
-            logger.Error("Generator not found in hideout areas.");
-            return;
-        }
+		if (generator == null)
+		{
+			logger.Error("Generator not found in hideout areas.");
+			return;
+		}
 
-        if (generator.Stages != null)
-            foreach (var stage in generator.Stages)
-                if (validStages != null)
-                    foreach (var validStage in validStages)
-                    {
-                        if (stage.Key != validStage)
-                        {
-                            logger.Error($"Stage {validStage} not found in generator fuel.");
-                            break;
-                        }
+		if (generator.Stages != null)
+			foreach (var validStage in validStages)
+			{
+				if (!generator.Stages.TryGetValue(validStage, out Stage? stage))
+				{
+					logger.Error($"Stage {validStage} not found in generator fuel.");
+					continue;
+				}
 
-                        if (stage.Value.Bonuses != null)
-                            foreach (var bonus in stage.Value.Bonuses)
-                            {
-                                if (bonus is not
-                                    { Type: BonusType.AdditionalSlots, Filter: { } filter }) continue;
-                                if (filter.Contains(itemId)) continue;
+				if (stage.Bonuses != null)
+					foreach (var bonus in stage.Bonuses)
+					{
+						if (bonus is not
+						    { Type: BonusType.AdditionalSlots, Filter: { } filter }) continue;
+						if (filter.Contains(itemId)) continue;
 
-                                filter.Add(itemId);
-                                LogHelper.Debug(logger,
-                                    $"[GeneratorFuel] Added item {itemId} as fuel to generator at stage with bonus ID {bonus.Id}");
-                            }
-                    }
-    }
+						filter.Add(itemId);
+						LogHelper.Debug(logger,
+							$"[GeneratorFuel] Added item {itemId} as fuel to generator at stage with bonus ID {bonus.Id}");
+					}
+			}
+	}
 }
