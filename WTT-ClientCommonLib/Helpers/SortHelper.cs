7532c90d3c34fb86fdd354b52c4afa06a5b11eea
peinwastaken <legokid173@gmail.com>
2026-02-26 10:52:08 +0200

    bunch of overkill sorthelper stuff

--- WTT-ClientCommonLib/Helpers/SortHelper.cs
+++ WTT-ClientCommonLib/Helpers/SortHelper.cs
@@ -1,13 +1,20 @@
+using EFT.InventoryLogic;
 using System;
+using System.Collections.Generic;
 using ItemSortOrderManager = GClass3381;
 
 namespace WTTClientCommonLib.Helpers
 {
     public static class SortHelper
     {
-        public static void AddToSortOrder(this Type itemType, Type itemToInsertAfter, int offset = 1)
+        public static void AddToSortOrder(this Type itemType, Type itemTypeToInsertAfter, int offset = 1)
         {
-            InsertAfter(itemType, itemToInsertAfter, offset);
+            InsertAfter(itemType, itemTypeToInsertAfter, offset);
+        }
+
+        public static int GetSortOrder(this Type itemType)
+        {
+            return ItemSortOrderManager.List_0.IndexOf(itemType);
         }
         
         public static void Insert(int sortIndex, Type itemType)
@@ -15,15 +22,38 @@ namespace WTTClientCommonLib.Helpers
             ItemSortOrderManager.List_0.Insert(sortIndex, itemType);
         }
 
+        public static void SetIndex(int newIndex, Type itemType)
+        {
+            List<Type> list = ItemSortOrderManager.List_0;
+            int oldIndex = list.IndexOf(itemType);
+
+            if (oldIndex < 0)
+            {
+                LogHelper.LogWarn($"failed to set sort index for item {itemType} because it does not exist in sort order list");
+                return;
+            }
+            
+            newIndex = Math.Clamp(newIndex, 0, list.Count - 1);
+            
+            list.RemoveAt(oldIndex);
+            
+            if (newIndex > oldIndex)
+            {
+                newIndex--;
+            }
+            
+            list.Insert(newIndex, itemType);
+        }
+
         public static void InsertAfter(Type itemType, Type itemToInsertAfter, int offset = 1)
         {
-            if (!ItemSortOrderManager.List_0.ContainsElement(itemToInsertAfter))
+            int itemIndex = ItemSortOrderManager.IndexOf(itemToInsertAfter);
+            
+            if (itemIndex < 0)
             {
                 LogHelper.LogWarn($"failed to add item type {itemType} to sort order because item type {itemToInsertAfter} doesn't exist in sort order list");
                 return;
             }
-            
-            int itemIndex = ItemSortOrderManager.IndexOf(itemToInsertAfter);
             
             ItemSortOrderManager.List_0.Insert(itemIndex + offset, itemType);
         }
